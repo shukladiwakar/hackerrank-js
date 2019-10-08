bed1460bba48a7dfcc50a74374e8de8e5b056e92
Diwakar Shukla <shukldiwakar@johndeere.com>
2019-10-08 18:51:10 +0530

    tag commit

---
+++ day3Array.js
@@ -0,0 +1,10 @@
+function getSecondLargest(nums) {
+    let i;
+    var secondLargest=0;
+    for (i; i < nums.size; i++) {
+        if(nums)
+
+    }
+}
+
+getSecondLargest([2, 3, 4, 1, 9, 21, 21])
